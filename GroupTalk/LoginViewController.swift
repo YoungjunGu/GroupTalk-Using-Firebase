0ca23852e63aa5346e914d3914114099140b7f41
gaki2745 <guyeongjun@naver.com>
2019-02-08 21:16:47 +0900

    Login MVC Patter 작업중

--- GroupTalk/LoginViewController.swift
+++ GroupTalk/LoginViewController.swift
@@ -10,15 +10,67 @@ import Firebase
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+
     //firebase의 내용을 원격으로 가져옴
     let remoteConfig = RemoteConfig.remoteConfig()
     var colorString: String! = nil
     
+    @IBAction func tapSignInAction(_ sender: Any) {
+        signInEvent()
+    }
     
+    @IBAction func tapSignUpAction() {
+        signUpPresent()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpLayout()
+        
+        //현재 유저가 로그인 중 인지 확인 하는 작업
+        if Auth.auth().currentUser != nil {
+            showAlert(message: "이미 로그인 된 상태입니다.")
+        }
+        
+    }
+
+}
+
+extension LoginViewController {
+    
+    func showAlert(message: String) {
+        
+        let alert = UIAlertController(title: "로그인 실패", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    fileprivate func signInEvent() {
+  
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: pwdTextField.text!) { (user, error) in
+            if user != nil{
+                print("login success")
+                self.signInButton.isHidden = true
+            }
+            else{
+                print("login fail")
+            }
+        }
+    
+    }
+    
+    fileprivate func signUpPresent() {
+        
+        let signUpVC = self.storyboard?.instantiateViewController(withIdentifier: "SignInViewController") as! SignUpViewController
+        self.present(signUpVC, animated: true, completion: nil)
+    }
+    
+    fileprivate func setUpLayout() {
+        
         let stateBar = UIView()
         self.view.addSubview(stateBar)
         stateBar.snp.makeConstraints{ (m) in
@@ -28,17 +80,7 @@ class LoginViewController: UIViewController {
         colorString = remoteConfig["splash_background"].stringValue
         
         stateBar.backgroundColor = UIColor(named: colorString)
-        loginButton.backgroundColor = UIColor(hex: colorString)
         signInButton.backgroundColor = UIColor(hex: colorString)
-        
-        //button action 연결
-        signInButton.addTarget(self, action: #selector(signUpPresent), for: .touchUpInside)
-        
+        signInButton.backgroundColor = UIColor(hex: colorString)
     }
-    
-    @objc func signUpPresent() {
-        let signUpVC = self.storyboard?.instantiateViewController(withIdentifier: "SignInViewController") as! SignUpViewController
-        self.present(signUpVC, animated: true, completion: nil)
-    }
-    
 }
---
+++ GroupTalk/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  GroupTalk
+//
+//  Created by youngjun goo on 08/02/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var email: String
+    var pwd: String
+    
+}
